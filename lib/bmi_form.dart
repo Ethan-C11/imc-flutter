aed5a2b54c44fd48487d3ff1518a45c6c1794df8
Ethan-C11 <ethan.collignon.1102@gmail.com>
2024-10-15 15:54:50 +0200

    etape 4

--- lib/bmi_form.dart
+++ lib/bmi_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:imc/text_input.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
 import './enum_type_input.dart';
 import './enum_imc_type.dart';
 import './body_mass_index.dart';
@@ -24,17 +25,20 @@ class BmiFormState extends State<BmiForm> {
       _taille = taille;
     });
   }
+
   void setPoids(int poids) {
     setState(() {
       _poids = poids;
     });
   }
+
   void setImc(double imc) {
     setState(() {
       _imc = imc;
     });
   }
-  void setImcType(EnumImcType imcType){
+
+  void setImcType(EnumImcType imcType) {
     setState(() {
       _imcType = imcType;
     });
@@ -42,17 +46,16 @@ class BmiFormState extends State<BmiForm> {
 
   @override
   Widget build(BuildContext context) {
-
     void onButtonPress(int? poids, int? taille) async {
       double imc = BodyMassIndex.calculerIMC(poids, taille);
-      if(imc == 0) {
+      if (imc == 0) {
         await showDialog(
             context: context,
             builder: (BuildContext context) {
               return AlertDialog(
                 title: const Text('Valeur incorrecte'),
                 content:
-                const Text('Veuillez remplir correctement les deux champs'),
+                    const Text('Veuillez remplir correctement les deux champs'),
                 actions: <Widget>[
                   TextButton(
                       onPressed: () {
@@ -79,25 +82,18 @@ class BmiFormState extends State<BmiForm> {
               Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: textInput(EnumTypeInput.poids, setPoids))
-            ]),          Padding(
+            ]),
+            Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: TextButton(
-                  onPressed: ()=>onButtonPress(_poids, _taille),
+                  onPressed: () => onButtonPress(_poids, _taille),
                   child: const Text("Calculer votre IMC"),
                 )),
             Builder(
               builder: (context) {
                 if (_imc != 0) {
-                  return Text('votre IMC est de ${_imc.toStringAsFixed(2)}.');
-                }
-                return const Text('');
-              },
-            ),
-            Builder(
-              builder: (context) {
-                if (_imc != 0) {
-                  switch(_imcType) {
-                    case EnumImcType.underweight :
+                  switch (_imcType) {
+                    case EnumImcType.underweight:
                       return const Text('Vous êtes en sous-poids');
                     case EnumImcType.normal:
                       return const Text('Vous êtes à un poids idéal');
@@ -105,18 +101,43 @@ class BmiFormState extends State<BmiForm> {
                       return const Text('Vous êtes en surpoids');
                     case EnumImcType.obesity:
                       return const Text('Vous êtes en obésité');
-                    default :
+                    default:
                       return const Text('');
                   }
                 }
                 return const Text('');
               },
             ),
+            SfRadialGauge(
+              axes: <RadialAxis>[
+                RadialAxis(
+                  minimum: 10,
+                  maximum: 40,
+                  ranges: [
+                    GaugeRange(
+                        startValue: 10, endValue: 18.5, color: Colors.blueGrey),
+                    GaugeRange(
+                        startValue: 18.5, endValue: 25, color: Colors.green),
+                    GaugeRange(
+                        startValue: 25, endValue: 30, color: Colors.orange),
+                    GaugeRange(startValue: 30, endValue: 40, color: Colors.red)
+                  ],
+                  pointers: [NeedlePointer(value: _imc)],
+                  annotations: [
+                    GaugeAnnotation(
+                      widget: Text(_imc.toStringAsFixed(2),
+                          style: const TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold)),
+                      positionFactor: 0.4,
+                    ),
+                  ],
+                )
+              ],
+            )
           ],
         ),
       );
     }
-
 
     return Scaffold(
         appBar: AppBar(
